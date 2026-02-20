633d24e66d82bd29324a60908ee2d79701271aa1
Evgenii Makarov <russia10.02@mail.ru>
2026-02-20 15:48:02 +0300

    справлен TimetableTest.java и обновлены классы Coach и TimeOfDay для совместимости с JUnit 5

--- src/main/java/ru/yandex/practicum/gym/TrainingSession.java
+++ src/main/java/ru/yandex/practicum/gym/TrainingSession.java
@@ -7,14 +7,20 @@ public class TrainingSession {
     private final DayOfWeek dayOfWeek;
     private final TimeOfDay timeOfDay;
 
-    public TrainingSession(Group group,
-                           Coach coach,
+    public TrainingSession(Coach coach,
                            DayOfWeek dayOfWeek,
                            TimeOfDay timeOfDay) {
         this.group = group;
         this.coach = coach;
         this.dayOfWeek = dayOfWeek;
         this.timeOfDay = timeOfDay;
+    }
+
+    public TrainingSession(Coach coach, DayOfWeek dayOfWeek, TimeOfDay timeOfDay, Group group, Coach coach1, DayOfWeek dayOfWeek1, TimeOfDay timeOfDay1) {
+        this.group = group;
+        this.coach = coach1;
+        this.dayOfWeek = dayOfWeek1;
+        this.timeOfDay = timeOfDay1;
     }
 
     public Group getGroup() {
